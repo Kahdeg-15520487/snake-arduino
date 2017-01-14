0adc252fad025497a20e7bc9d980c973d3f79d37
Themanwithno <nguyenhuyhoangto@gmail.com>
2017-01-14 17:02:42 +0700

    Add files via upload
    ok

---
+++ Snake_game/Snake_sc.h
@@ -0,0 +1,56 @@
+#include "Square.h"
+typedef struct Node 
+{
+    Square data;
+    Node *next; 
+};
+typedef Node *List;
+
+const Square cul(6,6);
+const Square cur(6,313);
+const Square cdl(233,6);
+const Square cdr(233,313);
+
+Node *Make_Node (Node *, Square);
+void Insert_first (List &, Square);
+void Init (List &L) {L=NULL;};
+void Insert_k (List &, Square , int);
+int Search (List , Square);
+void Del_first (List &, Square &);
+void Del_k (List &, Square &, int);
+
+Node *Make_Node (Node *P, Square x) 
+{
+    P = (Node *) malloc (sizeof (Node)); //Cap phat vung nho cho P
+    P->next = NULL; //Cho truong Next tro den NULL
+    P->data = x; //Ghi du lieu vao Data
+    return P; 
+}
+
+void Insert_first (List &L, Square x)  //Chen x vao vi tri dau tien trong danh sach
+{
+    Node *P; 
+    P = Make_Node(P,x); //tao 1 Node P
+    P->next = L; //Cho P tro den L
+    L = P; //L tro ve P
+}
+
+
+  
+class Snake
+{
+  private:
+  int n=4;
+  int dir=6;
+  List L;
+  Square head;
+  Square tail;
+  Square food;
+
+  public:
+  Snake();
+  ~Snake() {};
+
+  
+};
+
